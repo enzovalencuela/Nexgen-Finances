f575ac29d8e52c73cfebd3dda6e25e3d4315001b
enzovalencuela <esilvavalencuela@gmail.com>
2026-04-20 21:19:40 -0400

    fix: auth

--- auth.config.ts
+++ auth.config.ts
@@ -2,6 +2,8 @@ import type { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
 
 const authConfig = {
+  secret: process.env.AUTH_SECRET,
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID ?? "",
